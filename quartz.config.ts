378bc5a99ecddca26b7697d63aa9d47e34e4d477
Konrad Knap <konrad@MacBook-Pro-Konrad.local>
2026-04-19 19:36:44 +0200

    update baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "konkn.github.io/main_vault",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
